7daa5cd88249ed2fc1139aeecba3b4bb1919051d
Nikhil Thakur <nikhilthakur@Nikhils-MacBook-Air.local>
2023-05-15 17:55:44 -0400

    finished extracting data, got indictors and symbols database

---
+++ symbolGen.py
@@ -0,0 +1,34 @@
+import sqlite3
+import requests
+from symbolGenConfig import *
+
+# Crete db if it doesn't exist
+with sqlite3.connect('tradingview.db') as con:
+    db = con.cursor()
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS tv (screener TEXT, exchange TEXT, symbol TEXT, desc TEXT)")
+    con.commit()
+
+# Delete all rows
+with sqlite3.connect('tradingview.db') as con:
+    db = con.cursor()
+    db.execute("DELETE FROM tv")
+    con.commit()
+
+for types in ["", "\"futures\""]:
+    for x, _ in SCREENER.items():
+        print(f"Loading screener: {x}")
+        r = requests.post(f"https://scanner.tradingview.com/{x}/scan",
+                          data=f'{{"symbols":{{"tickers":[],"query":{{"types":[{types}]}}}},"columns":["description"]}}')
+        data = []
+        for res in r.json()["data"]:
+            exchange, symbol = res["s"].split(":")
+            desc = res["d"][0]
+            data.append((x, exchange, symbol, desc,))
+
+        # Use bulk operation for faster insert
+        with sqlite3.connect('tradingview.db') as con:
+            con.executemany(
+                "INSERT INTO tv VALUES (?, ?, ?, ?)",
+                data
+            )
--- trends.py
+++ trends.py
@@ -6,12 +6,14 @@ def get_trend(df):
     i = 0
     j = 1
 
-    while j < df.index:
+    while j < len(df):
         if "4" in inpereter(i, j, df):
             print("Error inpereting data")
         else:
-            trends["{}-{}".format(df.Date.iloc[i], df.Date.iloc[j])
-                   ]["trend"] = inpereter(i, j, df)
+            trends["{}-{}".format(df.index.tolist()[i], df.index.tolist()[j])
+                   ] = inpereter(i, j, df)
+        i += 1
+        j += 1
 
     return trends
 
@@ -26,11 +28,31 @@ def inpereter(i, j, df):
     pervious_open = df.Open.iloc[i]
     pervious_close = df.Close.iloc[i]
 
-    result = [4, 4, 4]
+    result = {}
+    trend = []
 
-    result[0] = helper_inpereter(pervious_open, pervious_close)
-    result[1] = helper_inpereter(pervious_close, open)
-    result[2] = helper_inpereter(open, close)
+    # getting trend
+    trend.append(helper_inpereter(pervious_open, pervious_close))
+    trend.append(helper_inpereter(pervious_close, open))
+    trend.append(helper_inpereter(open, close))
+    result["trend"] = trend
+
+    var = {open: "open", close: "close", pervious_open: "pervious_open",
+           pervious_close: "pervious_close"}
+
+    # getting max and min values
+    maximum = max(var)
+    result["maximum"] = (var.get(maximum), maximum)
+
+    # getting min value
+    minimum = min(var)
+    result["minimum"] = (var.get(minimum), minimum)
+
+    # print("open: ", open)
+    # print("close: ", close)
+    # print("pervious_open: ", pervious_open)
+    # print("pervious_close: ", pervious_close)
+    # print("result:", result)
 
     return result
 
@@ -44,4 +66,5 @@ def helper_inpereter(i, j):
         return 3
 
 
-print(get_trend(dataF))
+data = get_trend(dataF)
+print(data["{}-{}".format(dataF.index.tolist()[0], dataF.index.tolist()[1])])
